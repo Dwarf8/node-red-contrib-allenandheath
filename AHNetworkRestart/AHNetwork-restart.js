1442fac215e2839aeff71fa9de505ddc520ee48a
Dwarf8 <dwarf8@gmail.com>
2026-01-19 13:04:49 +0100

    Implement AHNetworkRestart node functionality

--- AHNetworkRestart/AHNetwork-restart.js
+++ AHNetworkRestart/AHNetwork-restart.js
@@ -1,1 +1,18 @@
+module.exports = function(RED) {
+    function AHNetworkRestart(config) {
+        RED.nodes.createNode(this, config);
+        const node = this;
+        const network = RED.nodes.getNode(config.network); // AHNetwork config node
 
+        node.on("input", function(msg) {
+            if (network && typeof network.restart === "function") {
+                network.restart();
+                node.status({fill:"green", shape:"dot", text:"Restarted"});
+            } else {
+                node.status({fill:"red", shape:"ring", text:"No network"});
+            }
+        });
+    }
+
+    RED.nodes.registerType("AHNetwork-restart", AHNetworkRestart);
+};
