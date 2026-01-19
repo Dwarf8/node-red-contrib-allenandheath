93492072b44b2b070cdf5ea636beb3d3e5bc8955
Dwarf8 <dwarf8@gmail.com>
2026-01-19 13:52:10 +0100

    Refactor functions and improve readability

--- functions/ahm/parameters.js
+++ functions/ahm/parameters.js
@@ -5,12 +5,15 @@ module.exports = {
                 allCall: [0xF0, 0x00, 0x00, 0x1A, 0x50, 0x12, 0x01, 0x00],
                 currentHeader: undefined
             },
+
             recieveBuffer: Buffer.from([]),
             processTimeout: undefined,
             syncActive: true,
             syncTimeout: undefined,
             waitingInit: [],
             totalChannelSelection: [64, 64, 32], //[inputs, zones, control groups]
+
+            // === Funciones ===
             functions: {
                 muteControl: require("./muteControl.js").object(),
                 faderLevel: require("./faderLevel.js").object(),
@@ -19,96 +22,108 @@ module.exports = {
                 sceneRecall: require("./sceneRecall.js").object(),
             },
 
-            //Reset this object
+            // === Reset ===
             reset: function () {
                 this.sysexHeader.currentHeader = undefined;
                 this.syncActive = true;
 
                 Object.keys(this.functions).forEach(function (key) {
-                    try { this.functions[key].reset(); }
-                    catch (e) { }
-                });
+                    try { 
+                        if(this.functions[key].reset) this.functions[key].reset(); 
+                    } catch (e) { }
+                }.bind(this));
             },
 
+            // === Recibir mensajes ===
             recieve: function (msg, midiChannel, server, callback) {
-                var object = this;
-                var value = [];
+                const object = this;
+                let value = [];
 
-                //Store the buffer
                 object.recieveBuffer = Buffer.concat([object.recieveBuffer, msg]);
 
-                //Process our buffer once we stop getting commands
+                // Procesar buffer después de 100ms
                 clearTimeout(object.processTimeout);
                 object.processTimeout = setTimeout(function () {
-                    //Find the function and process
                     Object.keys(object.functions).forEach(function (key) {
-                        var temp = object.functions[key].recieve(midiChannel, object.recieveBuffer, server);
-                        if (temp !== false && temp !== true) {
-                            value.push(temp);
-                        }
+                        const temp = object.functions[key].recieve
+                            ? object.functions[key].recieve(midiChannel, object.recieveBuffer, server)
+                            : false;
+                        if (temp !== false && temp !== true) value.push(temp);
                     });
                     callback(value);
                 }, 100);
 
+                // Procesar sync
                 clearTimeout(object.syncTimeout);
                 object.syncTimeout = setTimeout(function () {
-                    //Once we're done syncing send the values
-                    if (object.syncActive == true) {
+                    if (object.syncActive === true) {
                         if (object.waitingInit.length > 0) {
-                            object.functions[object.waitingInit.pop()].initial(server, midiChannel);
-                        }
-                        else {
+                            const fn = object.functions[object.waitingInit.pop()];
+                            if(fn && typeof fn.initial === "function") fn.initial(server, midiChannel);
+                        } else {
                             object.syncActive = false;
-                            var value = [];
+                            const finalValue = [];
                             Object.keys(object.functions).forEach(function (key) {
-                                var temp = object.functions[key].getData()
-                                if (temp !== false && temp !== true) {
-                                    value.push(temp);
+                                if(object.functions[key].getData) {
+                                    const temp = object.functions[key].getData();
+                                    if(temp !== false && temp !== true) finalValue.push(temp);
                                 }
                             });
-                            callback([value]);
+                            callback([finalValue]);
                         }
                     }
-
-                    //Clear the unused buffer
                     object.recieveBuffer = Buffer.from([]);
                 }, 3000);
 
                 return true;
             },
 
+            // === Generar paquete MIDI ===
             generatePacket: function (msg, server, midiChannel, returnPayload) {
-                var value = false;
-                var temp = this;
-                Object.keys(temp.functions).forEach(function (key) {
-                    if (value === false) {
-                        var func = temp.functions[key];
-                        value = func.generatePacket(msg, server, midiChannel, returnPayload);
+                let value = false;
+                Object.keys(this.functions).forEach(function (key) {
+                    if (value === false && this.functions[key].generatePacket) {
+                        value = this.functions[key].generatePacket(msg, server, midiChannel, returnPayload);
                     }
-                });
-
+                }.bind(this));
                 return value;
             },
 
+            // === Ping (dummy por ahora) ===
             sendPing: function (server, midiChannel, successFunction) {
+                if(successFunction) successFunction(false);
                 return false;
             },
 
-            //Send message on initial connection
+            // === Inicialización de la conexión ===
             initialConnection: function (server, midiChannel, callback) {
-                var temp = this;
-                Object.keys(this.functions).forEach(function (func) {
-                    var mode = temp.functions[func];
-                    mode.setup(temp);
+                const temp = this;
+
+                // Setup de todas las funciones
+                Object.keys(temp.functions).forEach(function (key) {
+                    const mode = temp.functions[key];
+                    if(mode.setup) mode.setup(temp);
                 });
 
-                //Request initial data for the first function
-                temp.functions[Object.keys(temp.functions)[0]].initial(server, midiChannel);
-
-                for (var func in temp.functions) {
-                    temp.waitingInit.push(func);
+                // Inicializar la primera función de manera segura
+                const keys = Object.keys(temp.functions);
+                if(keys.length > 0) {
+                    const firstFunc = temp.functions[keys[0]];
+                    if(firstFunc && typeof firstFunc.initial === "function") {
+                        firstFunc.initial(server, midiChannel);
+                    }
                 }
-                temp.functions[temp.waitingInit.pop()].initial(server, midiChannel);
+
+                // Llenar waitingInit y llamar a initial de cada función si existe
+                temp.waitingInit = Object.keys(temp.functions).slice(); // copia de keys
+                while(temp.waitingInit.length > 0) {
+                    const fnName = temp.waitingInit.pop();
+                    const fnObj = temp.functions[fnName];
+                    if(fnObj && typeof fnObj.initial === "function") {
+                        fnObj.initial(server, midiChannel);
+                    }
+                }
+
                 return true;
             }
         }
